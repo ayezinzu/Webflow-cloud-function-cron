b4432bb91adc9e04b78a7ad3acd0a601c1a535df
yogesh <ayezinzu@gmail.com>
2022-02-06 22:39:10 +0530

    update

--- netlify/functions/scheduled-function/scheduled-function.js
+++ netlify/functions/scheduled-function/scheduled-function.js
@@ -2,6 +2,9 @@ const { schedule } = require("@netlify/functions");
 const Axios = require("axios");
 const Webflow = require("webflow-api");
 let token = "80a56e479ccf514f73136b8ba7cee4a60eda31282b0c42a3ff46974e598713be";
+let presale = "a11c30f51c9513e981c789855d95aadf"
+let premarketing = "03047f81538f39913e74b08374d2f344"
+let openlocation = "be118a3cb31f7e7ca424b2dbcb98461a"
 const headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "Content-Type",
@@ -11,11 +14,14 @@ const headers = {
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
 module.exports.handler = schedule("* * * * *", async (event) => {
-  const eventBody = JSON.parse(event.body);
+  // const eventBody = JSON.parse(event.body);
+  // console.log(`Next function run at ${eventBody.next_run}.`);
+  let data_id = "61ffe6dd32d8317e817c834f"
   const webflow = new Webflow({ token: token });
-  const info = await webflow.info();
+  const info = await webflow.updateItem({collectionId: data_id, itemId: "61fff2a53387fe00f4549204", fields: {
+    "status": presale
+  }});
   console.log(info);
-  console.log(`Next function run at ${eventBody.next_run}.`);
 
   return {
     statusCode: 200,
